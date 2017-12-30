13d05d581d1e65f2964ea10c786bdb10df198898
aditya2222 <adityasingh222247@gmail.com>
2017-12-30 19:04:14 +0530

    newCommit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // hiding the dice
 document.querySelector('.dice').style.display = 'none';
@@ -24,11 +24,26 @@ document.getElementById('current-1').textContent = 0;
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
+    // Random Number
     var dice = Math.floor(Math.random()*6)+1;
-
+    // Display The Result
     var diceRoll = document.querySelector('.dice');
     diceRoll.src = 'dice-'+dice+'.png';
     diceRoll.style.display = 'block';
+    // Update the round score if the rolled number was not 1
+    if(dice > 1){
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-'+activePlayer).textContent = roundScore;
+    }else{
+        // Next Player turn
+        if (activePlayer==1){
+            activePlayer = 0;
+        }
+        else{
+            activePlayer = 1;
+        }
+    }
 
 
 
